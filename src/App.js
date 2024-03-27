7be74047b598a6f8ee27dbec8ac53457e785d37d
Kate <polyakovkate@gmail.com>
2024-03-26 23:21:09 -0700

    created branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 function App() {
   return (
     <div>
-      <p className="app">feature-01-partials branch. Typography works!!</p>
+      <p className="app">Navigation</p>
     </div>
   );
 }
