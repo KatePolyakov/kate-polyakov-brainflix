ddf8bac49d9e8322bdd6818596ad21e3a4edbd8d
Kate <polyakovkate@gmail.com>
2024-03-31 20:52:20 -0700

    styled

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -7,13 +7,20 @@ import { CommentInput } from '../CommentInput';
 export const Comments = ({ currentVideo }) => {
   return (
     <div className="comments">
-      <p className="comments__counts">{currentVideo.comments.length} comments</p>
+      <p className="comments__counts">{currentVideo.comments.length} Comments</p>
       <CommentInput />
       {currentVideo.comments.map((comment) => (
         <div key={comment.id} className="comments__exists">
-          <p>{comment.name}</p>
-          <p>{formatTime(comment.timestamp)}</p>
-          <p>{comment.comment}</p>
+          <div className="comments__img-group">
+            <div className="comments__img"></div>
+          </div>
+          <div className="comments__group">
+            <div className="comments__name-date">
+              <p className="comments__name">{comment.name}</p>
+              <p className="comments__date">{formatTime(comment.timestamp)}</p>
+            </div>
+            <p className="comments__comment">{comment.comment}</p>
+          </div>
         </div>
       ))}
     </div>
