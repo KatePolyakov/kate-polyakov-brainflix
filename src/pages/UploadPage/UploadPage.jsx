03151536f2918d5cd84acebb97e337f692790065
Kate <polyakovkate@gmail.com>
2024-04-07 23:35:18 -0700

    styled uploadPage and fix style in NavBar

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -6,32 +6,36 @@ import './uploadPage.scss';
 export const UploadPage = () => {
   return (
     <div className="upload-video">
-      <p className="upload-video__title">Upload Video</p>
+      <p className="upload-video__title-section">Upload Video</p>
       <form className="upload-video__form">
         <div className="upload-video__wrapper">
           <div className="upload-video__video-section">
-            <label className="upload-video__title">Video Thumbnail</label>
+            <label className="upload-video__title">VIDEO THUMBNAIL</label>
             <img src={UploadVideoPreview} className="upload-video__video" alt="thumbnail" />
           </div>
           <div className="upload-video__description-section">
-            <label className="upload-video__title">TITLE YOUR VIDEO</label>
-            <input
-              className="upload-video__input"
-              type="text"
-              placeholder="Add a title to your video"
-            />
-            <label className="upload-video__title">ADD A VIDEO DESCRIPTION</label>
-            <textarea
-              className="upload-video__textarea"
-              type="text"
-              placeholder="Add a description to your video"
-              rows={5}
-            />
+            <div className="upload-video__title-video">
+              <label className="upload-video__title">TITLE YOUR VIDEO</label>
+              <input
+                className="upload-video__input"
+                type="text"
+                placeholder="Add a title to your video"
+              />
+            </div>
+            <div className="upload-video__description-video">
+              <label className="upload-video__title">ADD A VIDEO DESCRIPTION</label>
+              <textarea
+                className="upload-video__textarea"
+                type="text"
+                placeholder="Add a description to your video"
+                rows={5}
+              />
+            </div>
           </div>
         </div>
-        <div className='upload-video__buttons'>
-          <button className='upload-video__cancel-button'>Cancel</button>
-          <button className='upload-video__publish-button'>Publish</button>
+        <div className="upload-video__buttons">
+          <button className="upload-video__cancel-button">CANCEL</button>
+          <button className="upload-video__publish-button">PUBLISH</button>
         </div>
       </form>
     </div>
